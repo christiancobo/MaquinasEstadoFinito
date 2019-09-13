3bda9188774b1875ea7a2373c41f354963962d62
Christian Andrés Cobo Ramiréz <53985343+christiancobo@users.noreply.github.com>
2019-09-13 18:58:52 -0500

    Modificación entidad Estado

--- src/modelo/Estado.java
+++ src/modelo/Estado.java
@@ -1,5 +1,18 @@
 package modelo;
 
-public class Estado {
+public class Estado{
 
+	private String nombre ;
+	
+	public Estado(String nombre ) {
+		this.nombre = nombre;
+	}
+
+	public String getnombre() {
+		return nombre;
+	}
+
+	public void setnombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
