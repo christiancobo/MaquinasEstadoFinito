95690dffba2703157920265a3ce344f722e15c5a
Christian Andrés Cobo Ramiréz <53985343+christiancobo@users.noreply.github.com>
2019-09-13 23:06:39 -0500

    Creación panel principal

---
+++ src/interfaz/PanelPrincipal.java
@@ -0,0 +1,29 @@
+package interfaz;
+
+import javax.swing.JFrame;
+
+public class PanelPrincipal  extends JFrame {
+	
+	
+	public PanelPrincipal() {
+		// TODO Auto-generated constructor stub
+		
+	
+		setTitle("Minimización: maquinas de estado finito");
+		setSize(600, 750);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		PanelPrincipal  panelPrincipal = new PanelPrincipal();
+		panelPrincipal.setVisible(true);
+	}
+
+}
